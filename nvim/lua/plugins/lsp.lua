b6f3c2a8da3f01e62464690799e54c656fb1e4ed
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-04-16 09:37:48 +0000

    style: better jdtls comment

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,81 +1,5 @@
 return {
   {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source.
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          if vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          --  See the README about individual language/framework/plugin snippets:
-          --  https://github.com/rafamadriz/friendly-snippets
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
-        },
-      },
-      'saadparwaiz1/cmp_luasnip',
-      -- Adds other completion capabilities.
-      -- nvim-cmp does not ship with all sources by default. They are split
-      -- into multiple repos for maintenance purposes.
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-    },
-    config = function()
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = {
-          completeopt = 'menu,menuone,noinsert',
-        },
-        mapping = cmp.mapping.preset.insert {
-          -- Select the next item.
-          ['<c-j>'] = cmp.mapping.select_next_item(),
-          -- Select the previous item.
-          ['<c-k>'] = cmp.mapping.select_prev_item(),
-          -- Accept the completion.
-          -- This will auto-import if your LSP supports it.
-          -- This will expand snippets if the LSP sent a snippet.
-          ['<c-space>'] = cmp.mapping.confirm { select = true },
-          -- Move to the next item of each of the expansion locations.
-          ['<c-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          -- Move to the previous item of each of the expansion locations.
-          ['<c-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-        },
-      }
-    end,
-  },
-  {
     'neovim/nvim-lspconfig',
     dependencies = {
       'williamboman/mason.nvim',
@@ -167,7 +91,7 @@ return {
                   {
                     name = 'JavaSE-1.8',
                     path = vim.fn.glob('/usr/local/sdkman/candidates/java/8*'),
-                    -- This field is required in order for jdtls to function with Java 8 projects.
+                    -- This field is required in order for jdtls to work with Java 8 projects.
                     default = true,
                   },
                   {
