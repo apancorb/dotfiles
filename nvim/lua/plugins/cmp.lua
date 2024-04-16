26b4f9792328a587f9c5dbbeae295f8a9fe9acf0
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-04-16 09:38:22 +0000

    feat: add completion to nvim

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,176 @@
+return {
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source.
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          if vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          --  See the README about individual language/framework/plugin snippets:
+          --  https://github.com/rafamadriz/friendly-snippets
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+      },
+      'saadparwaiz1/cmp_luasnip',
+      -- Adds other completion capabilities.
+      -- nvim-cmp does not ship with all sources by default. They are split
+      -- into multiple repos for maintenance purposes.
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'onsails/lspkind-nvim',
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+      luasnip.config.setup({})
+
+      local lspkind_comparator = function(conf)
+        local lsp_types = require('cmp.types').lsp
+        return function(entry1, entry2)
+          if entry1.source.name ~= 'nvim_lsp' then
+            if entry2.source.name == 'nvim_lsp' then
+              return false
+            else
+              return nil
+            end
+          end
+          local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
+          local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
+
+          local priority1 = conf.kind_priority[kind1] or 0
+          local priority2 = conf.kind_priority[kind2] or 0
+          if priority1 == priority2 then
+            return nil
+          end
+          return priority2 < priority1
+        end
+      end
+
+      local label_comparator = function(entry1, entry2)
+        return entry1.completion_item.label < entry2.completion_item.label
+      end
+
+      local cmp_kinds = {
+        Text = ' ',
+        Method = ' ',
+        Function = ' ',
+        Constructor = ' ',
+        Field = ' ',
+        Variable = ' ',
+        Class = ' ',
+        Interface = ' ',
+        Module = ' ',
+        Property = ' ',
+        Unit = ' ',
+        Value = ' ',
+        Enum = ' ',
+        Keyword = ' ',
+        Snippet = ' ',
+        Color = ' ',
+        File = ' ',
+        Reference = ' ',
+        Folder = ' ',
+        EnumMember = ' ',
+        Constant = ' ',
+        Struct = ' ',
+        Event = ' ',
+        Operator = ' ',
+        TypeParameter = ' ',
+      }
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        completion = {
+          completeopt = 'menu,menuone,noinsert',
+        },
+        mapping = cmp.mapping.preset.insert {
+          -- Select the next item.
+          ['<c-j>'] = cmp.mapping.select_next_item(),
+          -- Select the previous item.
+          ['<c-k>'] = cmp.mapping.select_prev_item(),
+          -- Accept the completion.
+          -- This will auto-import if your LSP supports it.
+          -- This will expand snippets if the LSP sent a snippet.
+          ['<c-space>'] = cmp.mapping.confirm { select = true },
+          -- Move to the next item of each of the expansion locations.
+          ['<c-l>'] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { 'i', 's' }),
+          -- Move to the previous item of each of the expansion locations.
+          ['<c-h>'] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { 'i', 's' }),
+        },
+        sorting = {
+          comparators = {
+            lspkind_comparator({
+              kind_priority = {
+                Field = 11,
+                Property = 11,
+                Constant = 10,
+                Enum = 10,
+                EnumMember = 10,
+                Event = 10,
+                Function = 10,
+                Method = 10,
+                Operator = 10,
+                Reference = 10,
+                Struct = 10,
+                Variable = 9,
+                File = 8,
+                Folder = 8,
+                Class = 5,
+                Color = 5,
+                Module = 5,
+                Keyword = 2,
+                Constructor = 1,
+                Interface = 1,
+                Snippet = 0,
+                Text = 1,
+                TypeParameter = 1,
+                Unit = 1,
+                Value = 1,
+              },
+            }),
+            label_comparator,
+          },
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'path' },
+          { name = 'luasnip' },
+        },
+        formatting = {
+          format = function(_, vim_item)
+            vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
+            return vim_item
+          end,
+        },
+        experimental = {
+          ghost_text = true,
+        },
+      }
+    end,
+  },
+}
