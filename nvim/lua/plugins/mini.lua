db3a3598a49d2ed4a69f666c81131851603a4cfd
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-04-16 10:32:02 +0000

    feat: add mini comments and pairs

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,20 +1,23 @@
 return {
   {
-    'tpope/vim-commentary'
-  },
-  {
     'echasnovski/mini.nvim',
     config = function()
       -- Better Around/Inside textobjects.
       require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc).
+      -- Comment lines
+      require('mini.comment').setup()
+
+      -- Add/delete/replace surroundings
       require('mini.surround').setup()
 
+      -- Insert automatic pairings
+      require('mini.pairs').setup()
+
       -- Simple and easy statusline.
-      local statusline = require 'mini.statusline'
+      local statusline = require('mini.statusline')
       -- Set use_icons to true if you have a Nerd Font.
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.setup({ use_icons = vim.g.have_nerd_font })
       -- Cursor location to LINE:COLUMN.
       statusline.section_location = function()
         return '%2l:%-2v'
